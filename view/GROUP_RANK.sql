42197dae4a49d32af274888460baf0a7724b5bf0
Mizuki0419 <naked.gk01@gmail.com>
2026-07-19 05:25:01 +0900

    20260719

--- view/GROUP_RANK.sql
+++ view/GROUP_RANK.sql
@@ -6,27 +6,82 @@ DROP VIEW IF EXISTS GROUP_RANK;
 
 CREATE VIEW GROUP_RANK AS
 SELECT
-    GROUPSTAGE.group_name AS グループ,
-    GROUP_RANK3.rank AS 順位,
-    CASE
-        WHEN GROUP_RANK3.rank < 3 THEN '突破'
-        WHEN EIGHT_OF_THIRD.rank < 9 THEN '突破'
-        ELSE '敗退'
-    END AS 結果,
-    TEAM.team_name AS 'チーム',
-    GROUP_RANK3.played AS '試合数',
-    GROUP_RANK3.win AS '勝',
-    GROUP_RANK3.draw AS '分',
-    GROUP_RANK3.loss AS '負',
-    GROUP_RANK3.goal_scored AS '総得点',
-    GROUP_RANK3.goal_allowed AS '総失点',
-    GROUP_RANK3.goal_difference AS '得失点差',
-    GROUP_RANK3.conduct_score AS 'TCS',
-    GROUP_RANK3.points AS '勝ち点',
-    GROUP_RANK3.fifarank AS 'FIFAランク',
-    EIGHT_OF_THIRD.rank AS '3位チーム順位'
-FROM GROUP_RANK3
-LEFT OUTER JOIN GROUPSTAGE USING (group_id)
-LEFT OUTER JOIN EIGHT_OF_THIRD USING (team_id)
-LEFT OUTER JOIN TEAM USING (team_id)
+    "グループ",
+    "順位",
+    "結果",
+    "チーム",
+    "結果１",
+    "結果２",
+    "結果３",
+    "結果４",
+    "試合数",
+    " 勝 ",
+    " 分 ",
+    " 負 ",
+    "総得点",
+    "総失点",
+    "得失点差",
+    "TCS",
+    "勝ち点",
+    "FIFAランク",
+    "3位内順位"
+FROM (
+    SELECT
+        GROUPSTAGE.group_name AS "グループ",
+        '順位' AS "順位",
+        '結果' AS "結果",
+        'チーム' AS "チーム",
+        SCORE_SHEET.r1 AS "結果１",
+        SCORE_SHEET.r2 AS "結果２",
+        SCORE_SHEET.r3 AS "結果３",
+        SCORE_SHEET.r4 AS "結果４",
+        '試合数' AS "試合数",
+        '勝' AS " 勝 ",
+        '分' AS " 分 ",
+        '負' AS " 負 ",
+        '総得点' AS "総得点",
+        '総失点' AS "総失点",
+        '得失点差' AS "得失点差",
+        'TCS' AS "TCS",
+        '勝ち点' AS "勝ち点",
+        'FIFAランク' AS "FIFAランク",
+        '3位内順位' AS "3位内順位",
+        SCORE_SHEET.rkbn AS "rkbn"
+    FROM SCORE_SHEET
+    LEFT OUTER JOIN GROUPSTAGE USING (group_id)
+    WHERE SCORE_SHEET.rkbn = '1'
+    UNION ALL
+    SELECT
+        GROUPSTAGE.group_name AS "グループ",
+        GROUP_RANK3.rank AS "順位",
+        CASE
+            WHEN GROUP_RANK3.rank < 3 THEN '突破'
+            WHEN EIGHT_OF_THIRD.rank < 9 THEN '突破'
+            ELSE '敗退'
+        END AS "結果",
+        TEAM.team_name AS "チーム",
+        SCORE_SHEET.r1 AS "結果１",
+        SCORE_SHEET.r2 AS "結果２",
+        SCORE_SHEET.r3 AS "結果３",
+        SCORE_SHEET.r4 AS "結果４",
+        GROUP_RANK3.played AS "試合数",
+        GROUP_RANK3.win AS " 勝 ",
+        GROUP_RANK3.draw AS " 分 ",
+        GROUP_RANK3.loss AS " 負 ",
+        GROUP_RANK3.goal_scored AS "総得点",
+        GROUP_RANK3.goal_allowed AS "総失点",
+        GROUP_RANK3.goal_difference AS "得失点差",
+        GROUP_RANK3.conduct_score AS "TCS",
+        GROUP_RANK3.points AS "勝ち点",
+        GROUP_RANK3.fifarank AS "FIFAランク",
+        CASE WHEN GROUP_RANK3.rank = 3 THEN EIGHT_OF_THIRD.rank ELSE '-' END AS "3位内順位",
+        SCORE_SHEET.rkbn AS "rkbn"
+    FROM GROUP_RANK3
+    LEFT OUTER JOIN GROUPSTAGE USING (group_id)
+    LEFT OUTER JOIN EIGHT_OF_THIRD USING (team_id)
+    LEFT OUTER JOIN TEAM USING (team_id)
+    LEFT OUTER JOIN SCORE_SHEET USING (team_id)
+    WHERE SCORE_SHEET.rkbn = '2'
+)
+ORDER BY グループ, rkbn, 順位
 ;
--- view/SCORE_SHEET.sql
+++ view/SCORE_SHEET.sql
@@ -12,11 +12,11 @@ GRMT_FIL AS ( --グループステージの結果を抽出
     JOIN MATCH_TEAM USING (match_id)
     WHERE MATCHES.match_kbn = '1'
     ),
-GRMT_JOIN AS ( --チームごとの試合結果を1カラムに結合
+GRMT_JOIN AS ( --チームごとの試合結果を抽出（縦持ちかつ双方向）
     SELECT
         HOME.team_id AS team_id,
         AWAY.team_id AS team_id_away,
-        CONCAT(HOME.score, '-', AWAY.score) AS result
+        HOME.score || '-' || AWAY.score AS result
     FROM
         GRMT_FIL AS HOME
     INNER JOIN
@@ -24,45 +24,45 @@ GRMT_JOIN AS ( --チームごとの試合結果を1カラムに結合
         ON HOME.match_id = AWAY.match_id
         AND HOME.team_id != AWAY.team_id
     GROUP BY
-        HOME.team_id
+        HOME.match_id, HOME.team_id
     ),
-GRRANK AS ( --
+GRRANK AS ( --グループごとの順位を抽出
     SELECT
-        GROUP_RANK3.group_id
-        GROUP_RANK3.rank
+        GROUP_RANK3.group_id,
+        GROUP_RANK3.rank,
         GROUP_RANK3.team_id
-        CASE WHEN GROUP_RANK3.rank = 1 THEN result ELSE '-' END) AS A,
-        MAX(CASE WHEN team2 = 'B' THEN result ELSE '-' END) AS B,
-        MAX(CASE WHEN team2 = 'C' THEN result ELSE '-' END) AS C
     FROM GROUP_RANK3
-    JOIN TEAM USING (team_id)
-    GROUP BY GRMT_JOIN.team_id
-    ORDER BY group_id, GROUP_RANK3.rank
-
+    ),
+FIELD AS ( --グループごとの横軸を設定
+    SELECT
+        GRRANK.group_id AS group_id,
+        NULL AS rank,
+        NULL AS team_id,
+        MAX(CASE WHEN GRRANK.rank = 1 THEN GRR1.team_id ELSE 0 END) AS r1,
+        MAX(CASE WHEN GRRANK.rank = 2 THEN GRR2.team_id ELSE 0 END) AS r2,
+        MAX(CASE WHEN GRRANK.rank = 3 THEN GRR3.team_id ELSE 0 END) AS r3,
+        MAX(CASE WHEN GRRANK.rank = 4 THEN GRR4.team_id ELSE 0 END) AS r4,
+        '1' AS rkbn
+    FROM GRRANK
+    JOIN (SELECT * FROM GRRANK WHERE rank = 1) GRR1 USING (group_id)
+    JOIN (SELECT * FROM GRRANK WHERE rank = 2) GRR2 USING (group_id)
+    JOIN (SELECT * FROM GRRANK WHERE rank = 3) GRR3 USING (group_id)
+    JOIN (SELECT * FROM GRRANK WHERE rank = 4) GRR4 USING (group_id)
+    GROUP BY GRRANK.group_id
+    )
+SELECT * FROM FIELD
+UNION ALL
 SELECT
-    GRMT_JOIN.team_id AS team_id,
-    MAX(CASE WHEN GRMT_JOIN.team_id_away = 'A' THEN GRMT_JOIN.result ELSE '-' END) AS A,
-    MAX(CASE WHEN team2 = 'B' THEN result ELSE '-' END) AS B,
-    MAX(CASE WHEN team2 = 'C' THEN result ELSE '-' END) AS C
-FROM GRMT_JOIN
-GROUP BY GRMT_JOIN.team_id
-ORDER BY GRMT_JOIN.team_id
-
-
-
-
-
-
-
-SELECT
-    MAX(TEAM.group_id) AS group_id,
-    MAX(GROUP_RANK3.rank) AS rank,
-    GRMT_JOIN.team_id AS team_id,
-    MAX(CASE WHEN team2 = 'A' THEN result ELSE '-' END) AS A,
-    MAX(CASE WHEN team2 = 'B' THEN result ELSE '-' END) AS B,
-    MAX(CASE WHEN team2 = 'C' THEN result ELSE '-' END) AS C
-FROM GRMT_JOIN
-JOIN TEAM USING (team_id)
-GROUP BY GRMT_JOIN.team_id
-ORDER BY group_id, GROUP_RANK3.rank
+    GRRANK.group_id AS group_id,
+    GRRANK.rank,
+    GRRANK.team_id AS team_id,
+    MAX(CASE WHEN GRMT_JOIN.team_id_away = FIELD.r1 THEN GRMT_JOIN.result ELSE '-' END) AS r1,
+    MAX(CASE WHEN GRMT_JOIN.team_id_away = FIELD.r2 THEN GRMT_JOIN.result ELSE '-' END) AS r2,
+    MAX(CASE WHEN GRMT_JOIN.team_id_away = FIELD.r3 THEN GRMT_JOIN.result ELSE '-' END) AS r3,
+    MAX(CASE WHEN GRMT_JOIN.team_id_away = FIELD.r4 THEN GRMT_JOIN.result ELSE '-' END) AS r4,
+    '2' AS rkbn
+FROM GRRANK
+JOIN FIELD USING (group_id)
+LEFT OUTER JOIN GRMT_JOIN USING (team_id)
+GROUP BY GRRANK.team_id
 ;
